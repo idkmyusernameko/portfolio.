820cdd6a95ab030be339fd216c9d3c6286b2d06f
Mohammed Shayan <88719186+idkmyusernameko@users.noreply.github.com>
2024-11-02 23:52:17 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,20 @@
 import streamlit as st
-st.title("🎈 Mohammed Shayan.")
+st.title(" ✨ Mohammed Shayan.")
 st.subheader("A 12 year old Python developer.")
-st.columns = ['About Me','Social']
-about = st.write(''' ''')
+cols = st.columns(2)
+st.subheader('About me')
+about = cols[0]
+about.write('''Hello There! I am Mohammed Shayan 
+                         I am a 12 year old Python developer.
+                         I am currently learning in Class 7.
+                         I study in Devamatha which is situated in Kerala, India. 
+                         I started coding only a few months ago.
+                         So don't bully me😭 
+                      ''')
+st.subheader('Socials')
+social = cols[1]
+social.markdown(f'<a href="https://instagram.com/idkmyusernameko"><b>Instagram</b></a>', unsafe_allow_html=True)
+social.markdown(f'<a href="https://github.com/idkmyusernameko">GitHub</a>', unsafe_allow_html=True)
 st.balloons()
 st.write('Reach out to me')
-st.markdown(f'<a href="https://instagram.com/idkmyusernameko"><b>Instagram</b></a>', unsafe_allow_html=True)
-st.markdown(f'<a href="https://github.com/idkmyusernameko">GitHub</a>', unsafe_allow_html=True)
 st.write('I know this is trash so tell me the improvements I should make')
